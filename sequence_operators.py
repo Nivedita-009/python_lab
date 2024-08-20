9c1ddc928f8bd5d8d427a53dd4cc3681711ac745
Nivedita Dixit <anahitadixit@gmail.com>
2024-08-20 21:23:57 +0530

    Add files via upload

---
+++ sequence_operators.py
@@ -0,0 +1,15 @@
+string1 = "he's "
+string2 = "is not "
+string3 = "a good guy "
+string4 = "fjords"
+
+print(string1 + string2 + string3 + string4)
+print("he's "  "is not "  "a good guy "   "fjords " )
+print("hello "*(5+4))
+print("hello " *5 +"4")
+
+today="friday"
+print("day" in today)
+print("thu" in today)
+print("fri" in today)
+print("hy" in today)
