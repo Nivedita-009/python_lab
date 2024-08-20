db6f3d7ec04c82ef1a685ef63a0acf385ab103e8
Nivedita Dixit <anahitadixit@gmail.com>
2024-08-20 21:19:57 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,17 @@
+print("today is good day to learn python")
+print('hey are you "mohan" isnt you')
+print("hello" + "world!")
+print("how's you")
+greeting ="hello"
+name="nivedita"
+#if we want a space we can add that too
+print(greeting+name)
+print(greeting +' '+name)
+age=19
+print(age)
+print(type(greeting))
+print(type(age))
+age_in_words ="2 years"
+#can only concatenate str (not "int" )to str
+print(name+ f" is {age} years old")
+print(type(age))
