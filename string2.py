e007cf1a5d69f33620e22d54c4e45c559e0685cb
Nivedita Dixit <anahitadixit@gmail.com>
2024-08-20 21:20:54 +0530

    Add files via upload

---
+++ string2.py
@@ -0,0 +1,24 @@
+        # 012345678901234
+parrot = "norwegian blue"
+
+
+print(parrot[0:6])#norweg
+
+print(parrot[3:5])
+print(parrot[0:12])
+print(parrot[0:6:2]) #using a step in a slice   #nre
+print(parrot[0:6:3])#nw
+
+number= "9,123,134,999,444,378,678" #
+print(number[1::4])
+
+print(parrot[:9])
+print(parrot[10:14])
+print(parrot[10:])
+print(parrot[:6] + parrot[6:])
+print(parrot[:])
+
+        #01234567890123456789012345
+letters="abcdefghijklmnopqrstuvwxyz"
+print(letters[:7])
+print(letters[20:25])
