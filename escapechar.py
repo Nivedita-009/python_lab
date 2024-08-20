cd10909706a6f32db7a69342415e1c549b51e7fa
Nivedita Dixit <anahitadixit@gmail.com>
2024-08-20 21:25:39 +0530

    Add files via upload

---
+++ escapechar.py
@@ -0,0 +1,15 @@
+splitstring = "this string has been\nsplit over\nseveral\nlines"
+print(splitstring)
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+print('the pet shop owner said "No,no,\'e\'s uh,...he\'s resting".')
+#or
+print("the pet shop owner said\"No,no,'e's uh,...he's resting\".")
+print("""the pet shop owner said "No,no,'e's uh,...he's resting".""")
+anothersplitstring="""this string has been \
+split over \
+several \
+lines"""
+print(anothersplitstring)
+print("c:\\users\\niveditadixit\\notes.txt")
+print(r"c:\users\niveditadixit\notes.txt")
