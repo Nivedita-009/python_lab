6efb786a0cd772c3a3f827d6905b39f3675fbf26
Nivedita Dixit <anahitadixit@gmail.com>
2024-08-20 21:26:20 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,26 @@
+a=18
+b=9
+print(a+b)     #27
+print(a-b)    #9
+print(a*b)    #162
+print(a/b)    #2.0
+print(a//b)   # 2 integer division,rounded down towards minus infinity
+print(a%b)    # 0 modulo: the remainder after integer division
+
+print()
+for i in range(1,4):
+    print(i)
+print(a+b/3-4*12)
+print(a + (b / 3)-(4 * 12))
+print((((a+b)/3)-4)*12)
+
+c = a + b
+d = c / 3
+e = d - 4
+print(e * 12)
+
+print()
+print(a /(b*a)/b)
+
+g=7/2
+print (g)
