c5436da264938b5dd2a619b4604381e370d5d351
Nivedita Dixit <anahitadixit@gmail.com>
2024-08-20 21:24:35 +0530

    Add files via upload

---
+++ repfields.py
@@ -0,0 +1,20 @@
+age= 24
+print("my age is "+ str(age) + " years")
+print("my age is {0} years".format(age))
+print("there are {0} days in {1},{2},{3},{4},{5},{6} and {7}" .format(31,"jan","march","may","july","aug","oct","dec"))
+
+print("jan: {2},feb: {0},march: {2},april: {1},may: {2},june: {1},july: {2},aug: {2},sep: {1},oct: {2},nov: {1},dec: {2}".format(28,30,31))
+
+print()
+print("""jan: {2} 
+feb: {0}
+march: {2}
+april: {1}
+may: {2}
+june: {1}
+july: {2}
+aug: {2}
+sep: {1}
+oct: {2}
+nov: {1}
+dec: {2}""".format(28,30,31))
