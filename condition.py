580d6a0be03c6a200af675ecfeb6f4a59a8a8c1c
Nivedita Dixit <anahitadixit@gmail.com>
2024-08-23 20:49:10 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,8 @@
+age = int(input("how old are you?"))
+# if age>=16 and age<=65:     #simplified chained conversion
+if 16<=age<=65:
+    print("you did it")
+else:
+    print("you are looser")
+if age<16 or age>65:
+    print("you are good person")
